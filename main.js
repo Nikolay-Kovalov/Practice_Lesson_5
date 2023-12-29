c0685ab88396540afb393000e95ef553cd549fd7
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2023-12-29 20:20:00 +0200

    commit

---
+++ main.js
@@ -0,0 +1,49 @@
+let display = document.getElementById('display');
+
+
+function appendToDisplay(value) {
+
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error'
+    }
+}
+
+function calculateCos() {
+    let result = Math.cos(eval(display.value))
+    display.value = result.toFixed(2);
+}
+
+function calculateSin() {
+    let result = Math.sin(eval(display.value))
+    display.value = result.toFixed(2);
+}
+
+function calculateTan() {
+    let result = Math.tan(eval(display.value))
+    display.value = result.toFixed(2);
+}
+
+function calculateCot() {
+    let result = 1 / Math.tan(eval(display.value))
+    display.value = result.toFixed(2);
+}
+
+function calculateSqrt() {
+    let result = Math.sqrt(eval(display.value))
+    display.value = result.toFixed(2);
+}
+
+function calculateSquareRoot() {
+    let result = eval(display.value) ** 2;
+    display.value = result.toFixed(2);
+}
